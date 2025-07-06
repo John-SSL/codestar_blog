92b2608bcf90e2a3a1919f6418f21f2741d04dd6
John-SSL <jsscadden@gmail.com>
2025-07-06 20:20:18 +0100

    Fix bug in about/views.py

--- about/views.py
+++ about/views.py
@@ -3,6 +3,6 @@ from .models import About
 
 # Create your views here.
 def about_me(request):
-    queryset = About.objects.all().order_by('-updated_on').first()
+    about = About.objects.all().order_by('-updated_on').first()
 
-    return render(request,"about/about.html", {'about':about_me})
+    return render(request,"about/about.html", {"about":about},)
